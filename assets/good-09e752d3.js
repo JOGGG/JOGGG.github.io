6b3b306e4bd3efc63ac8ea650e1ea0b0614801b3
Jo Yang <joyang@datagile.cn>
2023-06-28 11:49:14 +0800

    2

---
+++ assets/good-09e752d3.js
@@ -0,0 +1,1 @@
+import{ar as e}from"./index-c283c009.js";function a(t){return e.get(`/goods/detail/${t}`)}function o(){return e.get("/categories")}function s(t){return e.get("/search",{params:t})}export{a,o as g,s};
